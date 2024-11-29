b6618b073fb34f6d2ed59c5d03524154fb7620c8
jotamath <01jmnc@gmail.com>
2024-11-29 11:46:25 -0300

    add arquivos backend

--- api/apps.py
+++ api/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class OjaomaConfig(AppConfig):
+class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'ojaoma'
+    name = 'api'
